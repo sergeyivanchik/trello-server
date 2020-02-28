ade520cf86a6535cfe7295404fd3123bab32c311
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-28 16:21:23 +0300

    Add board controller

---
+++ api/controllers/board.js
@@ -0,0 +1,30 @@
+// const mongoose = require('mongoose');
+const Board = require('../models/board');;
+
+
+async function boardList(req, res) {
+  await Board.find()
+    .then(boards => res.send(boards))
+    .catch(error => {
+      res.status(500).send({
+        message: error.message
+      });
+    });
+};
+
+async function addBoard(req, res) {
+  const newBoard = await new Board(req.body);
+  newBoard.save()
+    .then(board => res.send(board))
+    .catch(error => {
+      res.status(500).send({
+        message: error.message
+      });
+      res.send(error);
+    });
+};
+
+module.exports = {
+  boardList,
+  addBoard
+};
